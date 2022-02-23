dbf85d4c103d0523394f3ef001a47d6ae777aaa5
Lokesh23102002 <92876285+Lokesh23102002@users.noreply.github.com>
2022-02-23 13:05:23 +0530

    Add files via upload

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class calc(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=12)
+    rans = models.CharField(max_length=12)
+    rques = models.CharField(max_length=12)
+    birthday=models.DateField()
+    token = models.CharField( max_length=200,default='')
+    is_varified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    
+
+
+
+
+
+
